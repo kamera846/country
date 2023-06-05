bc811a23d581b78013ff0c192ed8bb613a944a8d
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-06 06:10:22 +0700

    style: setup search input component layout

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,18 @@
                 <h1 class="text">Country</h1>
             </div>
         </div>
+
+        <SearchDropdown
+            placeholder="Type any country name"
+            withIcon />
     </div>
 </template>
 
 <script>
+import SearchDropdown from "@/components/SearchDropdown.vue";
 export default {
-	name: "SearchEnginePage"
+	name: "SearchEnginePage",
+    components: { SearchDropdown }
 };
 </script>
 
