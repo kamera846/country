6a4f8ca3fcadb1b2bae2aec6116e41049e3a319f
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-06 14:36:25 +0700

    feat: search engine country

--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,71 @@
 
         <InputSearchDropdown
             placeholder="Type any country name"
-            withIcon />
+            withIcon
+            :isLoad="isLoad"
+            :items="countries"
+            @onInput="onInputHandler" />
 
     </div>
 </template>
 
 <script>
 import InputSearchDropdown from "@/components/inputs/InputSearchDropdown.vue";
+import { mapState, mapActions } from "vuex";
 
 export default {
 	name: "SearchEnginePage",
-    components: { InputSearchDropdown }
+    components: { InputSearchDropdown },
+    data() {
+        return {
+            isLoad: false,
+            countries: [],
+            debounceSearch: null,
+        }
+    },
+    computed: {
+        ...mapState({
+            countryState: (state) => state?.country
+        })
+    },
+    methods: {
+        ...mapActions({
+            getCountries: 'country/search',
+        }),
+        onInputHandler(data) {
+            this.isLoad = true
+
+            clearTimeout(this.debounceSearch)
+
+            this.debounceSearch = setTimeout(() => {
+                if (data) this.fetchCountries(data)
+                else {
+                    this.countries = []
+                    this.isLoad = false
+                }
+            }, 500);
+        },
+        fetchCountries(data) {
+
+            const params = { name: data, fields: 'name' }
+
+            this.getCountries(params).then(() => {
+
+                const response = this.countryState
+
+                if (response?.status === 404) {
+                    console.error(response?.message);
+                    this.countries = []
+                } else this.countries = response?.data?.slice(0, 5)
+                
+                this.isLoad = false
+
+            })
+        }
+    },
+    mounted() {
+
+    }
 };
 </script>
 
