3be7f9f965bf2ab85aa8b1d00f9be5691af87e30
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-06 18:38:08 +0700

    refactor: update country & callingcode vuex

--- store/api/v2/callingcode.js
+++ store/api/v2/callingcode.js
@@ -7,6 +7,6 @@ const BASE_URL = process?.env?.baseApiUrl + '/v2'
 */
 
 // In the api documentation the 'code' variable cannot be empty, I use '372' as the default value
-const callingCode = (code = 372) => axios.get(BASE_URL + `/callingcode/${ code }`)
+const callingCodeApi = (code = 372) => axios.get(BASE_URL + `/callingcode/${ code }`)
 
-export { callingCode }
+export { callingCodeApi }
