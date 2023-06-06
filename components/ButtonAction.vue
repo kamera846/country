9e6be2d23a428eda5c15d8392ef33ca3ffc5f384
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-06 09:32:55 +0700

    style: button action component

---
+++ components/ButtonAction.vue
@@ -0,0 +1,37 @@
+<template>
+    <nuxt-link class="btn-action" :to="to">
+        <div v-if="withIcon" class="btn-icon">
+            <ArrowLeftIcon />
+        </div>
+
+        <div class="btn-title">
+            <span>{{ title }}</span>
+        </div>
+    </nuxt-link>
+</template>
+
+<script>
+import ArrowLeftIcon from "@/components/icons/ArrowLeftIcon.vue"
+
+export default {
+    components: { ArrowLeftIcon },
+    props: {
+        to: {
+            type: String,
+            default: '/'
+        },
+        title: {
+            type: String,
+            default: 'Button'
+        },
+        withIcon: {
+            type: Boolean,
+            default: false
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/components/btn-action.scss';
+</style>
