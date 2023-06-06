10cbaa09c9dfce5d18c0e0a0bac962a59b916dca
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-06 13:00:44 +0700

    style: implement card country location component

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -4,12 +4,16 @@
         <div class="header wrapper space-before">
 
             <div class="button container">
+
+                <!-- Component Button Action -->
                 <ButtonAction
                     title="Back to Homepage"
                     withIcon />
+
             </div>
 
             <div class="container">
+
                 <div class="box-title">
                     <h1 class="country-name">{{ this.slug }}</h1>
                     <img class="country-flag" src="https://flagcdn.com/id.svg" alt="The flag of Indonesia is composed of two equal horizontal bands of red and white.">
@@ -22,27 +26,50 @@
                         <li class="badge badge-green">Republic of Indonesia</li>
                     </ul>
                 </div>
+
             </div>
 
         </div>
 
         <div class="body wrapper">
             <div class="container">
+                
                 <div class="box-overview">
+
+                    <!-- Component Card Country Location -->
+                    <CardCountryLocation
+                        :latLong="countryLatLong"
+                        :iconUrl="require('@/assets/images/svg/globe.svg')" />
+
+                    <!-- Component Card Country Location -->
+                    <CardCountryLocation :location="countryLocation" />
+
+                </div>
+
+            </div>
+
+            <div class="container">
+
+                <div class="box-overview">
+
+                    <!-- Component Card Country Overview -->
                     <CardCountryOverview
                         title="Calling Code"
                         content="62"
                         itemTitle="country"
-                        description="with this calling code"
-                    />
+                        description="with this calling code" />
+
+                    <!-- Component Card Country Overview -->
                     <CardCountryOverview
                         title="Currency"
                         content="IDR"
                         itemTitle="country"
-                        description="with this currency"
-                    />
+                        description="with this currency" />
+
                 </div>
+
             </div>
+
         </div>
 
     </div>
@@ -51,13 +78,27 @@
 <script>
 import ButtonAction from "@/components/buttons/ButtonAction.vue";
 import CardCountryOverview from "@/components/cards/CardCountryOverview.vue";
+import CardCountryLocation from "@/components/cards/CardCountryLocation.vue";
 
 export default {
 	name: "DetailCountryPage",
-    components: { ButtonAction, CardCountryOverview },
+    components: { ButtonAction, CardCountryOverview, CardCountryLocation },
     asyncData({params}) {
         const slug = params?.slug
         return { slug }
+    },
+    data() {
+        return {
+            countryLatLong: {
+                latitude: "-5.0",
+                longitude: "120.0"
+            },
+            countryLocation: [
+                { name: 'Capital', value: 'Jakarta' },
+                { name: 'Region', value: 'Asia' },
+                { name: 'Subregion', value: 'South-Eastern Asia' },
+            ]
+        }
     }
 };
 </script>
