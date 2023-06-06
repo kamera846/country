24eb0471b6644498d124cadbcd5adac31f200045
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-06 11:05:23 +0700

    style: card tooltip items component

---
+++ components/cards/CardTooltipItems.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card-tooltip-items">
+        <ul>
+            <li>Indonesia</li>
+            <li>Indonesia</li>
+            <li>Indonesia</li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/components/card-tooltip-items.scss';
+</style>
